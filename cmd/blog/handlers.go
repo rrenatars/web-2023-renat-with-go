835b6d0ba42b66d08ea4cb782b026f5ff70f68fb
rrenatars <renat.arslanov2015@yandex.ru>
2023-03-29 23:32:45 +0300

    Blog after go

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -9,11 +9,27 @@ import (
 type indexPage struct {
 	Title         string
 	FeaturedPosts []featuredPostData
+	MostRecent    []mostRecentData
 }
 
 type featuredPostData struct {
-	Title    string
-	Subtitle string
+	Title    				string
+	Subtitle           		string
+	BackgroundImageModifier string
+	AuthorAvatar   			string
+	AuthorName 				string
+	PublishDate 			string
+	PostButton 				string
+	ButtonClass				string
+}
+
+type mostRecentData struct {
+	PhotoImage 				string
+	Title 					string
+	Subtitle 				string
+	AuthorName 				string
+	AuthorAvatar 			string
+	PublishDate 			string
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +43,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	data := indexPage{
 		Title:         "Escape.",
 		FeaturedPosts: featuredPosts(),
+		MostRecent:    mostRecent(),
 	}
 
 	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
@@ -42,10 +59,75 @@ func featuredPosts() []featuredPostData {
 		{
 			Title:    "The Road Ahead",
 			Subtitle: "The road ahead might be paved - it might not be.",
+			BackgroundImageModifier: "featured__post_background-road",
+			AuthorAvatar: "../../static/images/mat_vogels.jpg",
+			AuthorName: "Mat Vogels",
+			PublishDate: "September 25, 2015",
+			PostButton: "",
+			ButtonClass: "",
 		},
 		{
 			Title:    "From Top Down",
-			Subtitle: "Once a year.",
+			Subtitle: "Once a year, go someplace you’ve never been before.",
+			BackgroundImageModifier: "featured__post_background-fromtop",
+			AuthorAvatar: "../../static/images/william_wong.jpg",
+			AuthorName: "William Wong",
+			PublishDate: "September 25, 2015",
+			PostButton: "ADVENTURE",
+			ButtonClass: "button__adventure",
+		},
+	}
+}
+
+func mostRecent() []mostRecentData {
+	return []mostRecentData{
+		{
+			PhotoImage: "../../static/images/still_standing_tall.jpg",
+			Title: "Still Standing Tall",
+			Subtitle: "Life begins at the end of your comfort zone.",
+			AuthorName: "William Wong",
+			AuthorAvatar: "../../static/images/william_wong.jpg",
+			PublishDate: "9/25/2015",
+		},
+		{
+			PhotoImage: "../../static/images/sunny_side_up.jpg",
+			Title: "Sunny Side Up",
+			Subtitle: "No place is ever as bad as they tell you it’s going to be.",
+			AuthorName: "Mat Vogels",
+			AuthorAvatar: "../../static/images/mat_vogels.jpg",
+			PublishDate: "9/25/2015",
+		},
+		{
+			PhotoImage: "../../static/images/water_falls.jpg",
+			Title: "Water Falls",
+			Subtitle: "We travel not to escape life, but for life not to escape us.",
+			AuthorName: "William Wong",
+			AuthorAvatar: "../../static/images/william_wong.jpg",
+			PublishDate: "9/25/2015",
+		},
+		{
+			PhotoImage: "../../static/images/through_the_mist.jpg",
+			Title: "Through the Mist",
+			Subtitle: "Travel makes you see what a tiny place you occupy in the world.",
+			AuthorName: "William Wong",
+			AuthorAvatar: "../../static/images/william_wong.jpg",
+			PublishDate: "9/25/2015",
+		},
+		{
+			PhotoImage: "../../static/images/still_standing_tall.jpg",
+			Title: "Still Standing Tall",
+			Subtitle: "Life begins at the end of your comfort zone.",
+			AuthorName: "William Wong",
+			AuthorAvatar: "../../static/images/william_wong.jpg",
+			PublishDate: "9/25/2015",
+		},
+		{
+			PhotoImage: "../../static/images/awaken_early.jpg",
+			Title: "Awaken Early",
+			Subtitle: "Not all those who wander are lost.",
+			AuthorName: "Mat Vogels",
+			AuthorAvatar: "../../static/images/mat_vogels.jpg",
+			PublishDate: "9/25/2015",
 		},
 	}
 }
